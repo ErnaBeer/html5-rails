07481bc196353dbc9fff3b3f665163569fe1432d
ErnaBeer <ugpftjamrd@outlook.com>
2024-01-12 00:39:26 +0800

    inventore

--- app/assets/javascripts/polyfills.js
+++ app/assets/javascripts/polyfills.js
@@ -1,6 +1,7 @@
-// This is a manifest for javascript polyfills that will be included
+//*This is a manifest for javascript polyfills that will be included
 // in the head section of your layouts.
 //
 // polyfill (n): a JavaScript shim that replicates the standard API for older browsers.
 //
 //= require modernizr.min
+ 
